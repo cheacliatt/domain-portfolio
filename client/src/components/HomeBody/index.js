aee2f41e1adc57f50b8489fe75d0f66aaac0b086
CheaC <cheacliatt@gmail.com>
2020-10-08 16:44:51 -0400

    Updated About Me

--- client/src/components/HomeBody/index.js
+++ client/src/components/HomeBody/index.js
@@ -17,32 +17,33 @@ function HomeBody() {
           <img src={Profile} alt="Me" id="chea-profile" />
           <p>
             I am a Full Stack Web and Software Developer, certified through the
-            Georgia Institute of Technology. My current skillset includes:
-            JavaScript, jQuery, React, Node, Express, MongoDB, and SQL
-            databases.
+            Georgia Institute of Technology. My current skillset includes
+            JavaScript application development with Object Oriented Programming,
+            jQuery, React, Node, Express, MongoDB and SQL databases. I am also
+            comfortable with participating with Agile practices.
           </p>
           <p>
-            I am a Georgia native, originally from Augusta. My hobbies are
-            trail-riding on my mountain bike, reading books (currently going
-            through the Dune series), and spoiling my two cats. Currently, I am
-            seeking opportunities to grow a career in web development around the
-            Atlanta area.
+            I am a Georgia native, originally from Augusta, but now I reside in
+            metro Atlanta. My hobbies are trail-riding on my mountain bike,
+            reading books (currently going through the Dune series), and
+            spoiling my two cats. Currently, I am seeking opportunities to grow
+            a career in web development around the Atlanta area.
           </p>
           <p>
-            My field of work has varied the last ten years, from teacher to
-            librarian, bartender and restaurant manager. Despite my history
-            working within customer service, one of my favorite pastimes is
-            working with computers and learning about technology. It is with
-            this raw appreciation for understanding new and evolving
-            technologies that makes me a prime canidate to work on projects, as
-            I aim to grow alongside my career.
+            Currently, I am employed as a Systems Adminstrator for American
+            Global Wealth Services, where I primarily create and update workflow
+            boards, facilitate transfer of Excel spreadsheets to SQL database
+            structures, and varied Front-End development tasks. I am also
+            currently an intern for a startup enterprise application called Sky
+            Retro, where I contribute on both Front-End and Back-End
+            development. As you can see, I like to stay busy!
           </p>
           <p>
             I have completed a Full Stack Development course offered by the
             Georgia Institute of Technology, receiving a 4.0 for my exemplary
-            work. My academic background also includes a Bacherlor of Arts in English
-            Composition, with side studies in Computer Science, and a Master of
-            Arts in Literature.
+            work. My academic background also includes a Bacherlor of Arts in
+            English Composition, with side studies in Computer Science, and a
+            Master of Arts in Literature.
           </p>
         </segment>
         <div class="col-sm-1"></div>
